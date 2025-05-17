2a0646cc90664d776db1efb4f14c82287ead27bd
Łukasz Janiszewski <ljaniszewski00@gmail.com>
2025-05-17 10:32:32 +0100

    Initial Commit

---
+++ RMGuideTCA/RMGuideTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMGuideTCAApp.swift
+//  RMGuideTCA
+//
+//  Created by Łukasz Janiszewski on 21/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct RMGuideTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
